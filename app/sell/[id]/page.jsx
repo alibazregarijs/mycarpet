bfc4452fc9201f97ef864d3886bbae302141521b
unknown <alibazregariintel@gmail.com>
2024-09-23 14:50:10 +0330

    sell css is complete for lg screen

--- app/sell/[id]/page.jsx
+++ app/sell/[id]/page.jsx
@@ -63,26 +63,40 @@ const Home = () => {
               <div className="col-span-10 row-span-12 bg-myWhite rounded-2xl ">
                 <Navbar />
                 <Hero />
-                <div className="grid grid-cols-12 gap-4 row-span-12 brder">
-                  <div className="col-span-5 border items-center justify-center ">
+                <div className="grid grid-cols-12 gap-4 row-span-12 ">
+                  <div className="col-span-5 mx-5 text-center items-center justify-center ">
                     <Image
-                      className="h-96 w-full mx-5 mt-3 rounded-md  shadow-none transition-shadow duration-300 cursor-pointer hover:shadow-lg hover:shadow-gray-400"
+                      className="h-96 w-full  mt-3 rounded-md  shadow-none transition-shadow duration-300 cursor-pointer hover:shadow-lg hover:shadow-gray-400"
                       src={carpet1}
                       width={0}
                       height={0}
                       alt={""}
                     />
                   </div>
-                  <div className="col-span-7 text-center mt-2  items-center justify-center">
-                    <div className="flex flex-col space-y-5 items-center justify-center ">
-                      <h1 className=" font-bold text-2xl">
+                  <div className="col-span-7 text-center mt-2  items-center justify-center ">
+                    <div className="flex flex-col space-y-10 items-center justify-center ">
+                      <h1 className="  hover:A91D3A text-black  py-2 px-4 border border-myRed rounded-full shadow-lg font-bold text-2xl">
                         Buy Without Any Hesitate
                       </h1>
-                      <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Provident, tempora distinctio voluptatibus earum nam
-                        odio! Natus facere amet sunt tenetur.
+                      <h3 className="font-bold text-xl text-transform: uppercase">
+                        The Best Carpet Gallery In Iran We Produce The Best
+                        Carpets
+                      </h3>
+
+                      <p className="text-center">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing
+                        elit. Similique harum omnis hic alias ipsum asperiores
+                        id, voluptate magni reprehenderit ipsam qui possimus
+                        recusandae! Fuga nemo natus vel necessitatibus adipisci
+                        velit provident veniam, quas voluptates possimus magnam
+                        error recusandae, deserunt eaque, a eveniet temporibus.
+                        Vel quibusdam ipsa assumenda cum, tempore consectetur
+                        quia autem debitis possimus? Reiciendis perspiciatis
+                        dolorem excepturi a repellat!
                       </p>
+                      <button className="bg-myRed hover:A91D3A text-white font-semibold py-2 px-4 border border-myRed rounded shadow">
+                        Buy This Carpet Now !
+                      </button>
                     </div>
                   </div>
                 </div>
